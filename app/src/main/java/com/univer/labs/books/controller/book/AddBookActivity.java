d9500b42e456949fd55979c2941b66a5cd6999f6
TheMichelangelo <mikepastula@gmail.com>
2020-05-10 22:46:27 +0300

    finalize add book functionality

--- app/src/main/java/com/univer/labs/books/controller/book/AddBookActivity.java
+++ app/src/main/java/com/univer/labs/books/controller/book/AddBookActivity.java
@@ -3,13 +3,19 @@ package com.univer.labs.books.controller.book;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.R.style;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.univer.labs.books.R;
 import com.univer.labs.books.model.Author;
@@ -17,6 +23,7 @@ import com.univer.labs.books.service.AuthorService;
 import com.univer.labs.books.service.BookService;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import static android.R.layout.simple_spinner_item;
@@ -26,8 +33,9 @@ public class AddBookActivity extends AppCompatActivity {
     AuthorService authorService;
 
     private Spinner authorSpinner;
-    private Button btnSubmit;
-    EditText datePublishedEdit,priceEdit,pagesCountEdit,bookNameEdit;
+    private TextView datePublishedEdit;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    EditText priceEdit, pagesCountEdit, bookNameEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +47,35 @@ public class AddBookActivity extends AppCompatActivity {
         pagesCountEdit = findViewById(R.id.add_book_pages_edit);
         priceEdit = findViewById(R.id.add_book_price_edit);
         datePublishedEdit = findViewById(R.id.add_book_date_edit);
+        datePublishedEdit.setText("11/22/1963");
         addAuthorItemsOnSpinner();
+
+        datePublishedEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddBookActivity.this,
+                        style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = month + "/" + day + "/" + year;
+                datePublishedEdit.setText(date);
+            }
+        };
 
     }
 
@@ -53,25 +89,33 @@ public class AddBookActivity extends AppCompatActivity {
         authorSpinner.setAdapter(dataAdapter);
     }
 
-    public void onAddClick(View v)
-    {
-        String bookNameText =bookNameEdit.getText().toString();
-        if(bookNameText.isEmpty())
+    public void onAddClick(View v) {
+        String bookNameText = bookNameEdit.getText().toString();
+        if (bookNameText.isEmpty()) {
+            showAlert("Book name can't be empty", "Something go wrong");
+            return;
+        }
+        String date = datePublishedEdit.getText().toString();
+        String priceText = priceEdit.getText().toString();
+        String pagesCount = pagesCountEdit.getText().toString();
+        if(priceText.isEmpty() || pagesCount.isEmpty())
         {
-            showAlert("Book name can't be empty","Something go wrong");
+            showAlert("Book price and pages count can't be empty", "Something go wrong");
             return;
         }
         Author author = (Author) authorSpinner.getSelectedItem();
-        boolean result=bookService.insertBook(bookNameText);
-        if(result)
-        {
-            showAlert("Book added","Success");
-            ]bookNameEdit.setText("");
+
+        boolean result = bookService.insertBook(bookNameText, date, Integer.valueOf(pagesCount), Double.valueOf(priceText), author.getAuthorId());
+        if (result) {
+            showAlert("Book added", "Success");
+            bookNameEdit.setText("");
+            datePublishedEdit.setText("11/22/1963");
+            priceEdit.setText("");
+            pagesCountEdit.setText("");
         }
     }
 
-    private void showAlert(String message,String title)
-    {
+    private void showAlert(String message, String title) {
         AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
         builder1.setTitle(title);
         builder1.setMessage(message);
