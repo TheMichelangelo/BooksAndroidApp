71c6eda6cd4a0e038e4e7a8566eeb0f1fbf03382
TheMichelangelo <mikepastula@gmail.com>
2020-05-11 19:41:14 +0300

    create books by year char templete

---
+++ app/src/main/java/com/univer/labs/books/controller/book/BooksByYearChartActivity.java
@@ -0,0 +1,16 @@
+package com.univer.labs.books.controller.book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.univer.labs.books.R;
+
+public class BooksByYearChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_by_year_chart);
+    }
+}
